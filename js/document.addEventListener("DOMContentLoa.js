23b2493b962d96a4f2ec2fcce03f86e6bd449aee
vicbentos11 <victoriabentos11@gmail.com>
2021-08-26 20:15:23 -0300

    fg

--- js/document.addEventListener("DOMContentLoa.js
+++ js/document.addEventListener("DOMContentLoa.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(e){
 
     document.getElementById("logButton").onclick = function(e) {
-        sessionStorage.setItem("user_id", document.getElementById("SU ID DEL IMPUT USUARIO").value)
+        localStorage.setItem("user_id", document.getElementById("SU ID DEL IMPUT USUARIO").value)
         
     };
 });
