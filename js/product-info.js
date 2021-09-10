bf6fe049e146ef602c5beb579d2c5727d4c46f20
vicbentos11 <victoriabentos11@gmail.com>
2021-09-09 21:12:27 -0300

    cambios para entrega 3

--- js/product-info.js
+++ js/product-info.js
@@ -30,15 +30,13 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             let productNameHTML  = document.getElementById("productName");
             let productDescriptionHTML = document.getElementById("productDescription");
-            let productCostHTML = document.getElementById("productCost");
             let productCurrencyHTML = document.getElementById("productCurrency");
             let productCategoryHTML = document.getElementById("productCategory");
             let productsoldCountHTML = document.getElementById("productSoldCount");
         
             productNameHTML.innerHTML = products.name;
             productDescriptionHTML.innerHTML = products.description;
-            productCostHTML.innerHTML = products.cost;
-            productCurrencyHTML.innerHTML = products.currency;
+            productCurrencyHTML.innerHTML = products.currency + " " + products.cost;
             productCategoryHTML.innerHTML = products.category;
             productsoldCountHTML.innerHTML = products.soldCount;
 
@@ -47,3 +45,48 @@ document.addEventListener("DOMContentLoaded", function(e){
         }
     });
 });
+
+
+fetch(PRODUCT_INFO_COMMENTS_URL)
+.then(data => data.json())
+.then(info => {
+ 
+    for(let i=0; i < info.length; i++){
+        let comments = info[i];
+
+        let score = comments.score;
+        let description = comments.description;
+        let user = comments.user
+        let date = comments.dateTime;
+
+        var stars = ""; //Poniendo las estrellas en comentarios
+
+        for(var estrellas = 0; estrellas < score; estrellas++)
+            stars += '<span class ="fa fa-star" style="color: orange"/>';
+        
+
+        let comentarios = "" ;
+        comentarios += `
+        <a class="list-group-item list-group-item-action">
+            <div class="row">
+                <div class="col">
+                    <div class="d-flex w-100 justify-content-between">
+                        <h4 class="mb-1">`+ stars +`</h4>
+                        <small class="text-muted">` + user + `</small>
+                    </div>
+                    <div class="d-flex w-100 justify-content-between">
+                    <h4 class="mb-1"></h4>
+                    <small class="text-muted">` + date + `</small>
+                    </div>
+                    <small class="text-muted">` + description + `</small>
+                </div>
+            </div>
+        </a>
+        `;
+        document.getElementById("comments-list-container").innerHTML += comentarios;
+     
+    }
+    });
+
+
+
